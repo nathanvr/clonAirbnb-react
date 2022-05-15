0819c2be33e155135dd05f0accb63555b3be2561
nathanvr <jonathanvaro1004@gmail.com>
2022-05-14 21:30:55 -0500

    adds the modal with styles and adds the icon and the name in the tab

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,26 +2,46 @@ import '../styles/components/LoginModal.scss';
 import Modal from 'react-modal/lib/components/Modal';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  faFacebook,
+  faGoogle,
+  faApple,
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import BrandIcon from './BrandIcon';
 
 Modal.setAppElement('#root');
 
 const LoginModal = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const customStyles = {
+    content: {
+      position: 'absolute',
+      margin: 'auto',
+      width: '400px',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+    },
+  };
   return (
     <div>
       <Link to="#" onClick={() => setModalOpen(true)}>
         Registro
       </Link>
-      <Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)}>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        style={customStyles}>
         <div className="loginFormContainer">
           <div className="loginForm__header">
             <button onClick={() => setModalOpen(false)}> X </button>
-            <h2>Te damos la bienvenida a airbnb</h2>
+            <h2>Iniciar sesion o registrarse</h2>
           </div>
+          <div></div>
           <form>
             <div className="form-row">
               <div className="form__button__country">
-                <label for="country__select">
+                <label>
                   <div>
                     <div className="form__button__country--text">
                       Pais/Region
@@ -30,7 +50,7 @@ const LoginModal = () => {
                 </label>
                 <div className="form-group">
                   <select id="country__select" className="form__group__select">
-                    <option value="1" selected>
+                    <option value="1" defaultValue>
                       Estados Unidos (+1)
                     </option>
                     <option value="2">Afganistan (+93)</option>
@@ -39,9 +59,9 @@ const LoginModal = () => {
                 </div>
               </div>
 
-              <div className="form-group">
+              <div className="form__group__label">
                 <input
-                  className="form-control"
+                  className="form__control--label"
                   placeholder="Numero de telefono"
                 />
               </div>
@@ -55,12 +75,14 @@ const LoginModal = () => {
             </div>
 
             <div className="form__button__continue">
-              <button type="submit" className="btn btn-danger">
-                <strong>Continua</strong>
-              </button>
+              <button className="form__button--continue">Continua</button>
             </div>
           </form>
-          <div className="dropdown-divider">o</div>
+          <div className="sectioner">
+            <div className="section__divider" />
+            <p>o</p>
+            <div className="section__divider" />
+          </div>
 
           <div className="form__buttons__sm">
             <button
@@ -68,10 +90,14 @@ const LoginModal = () => {
               className="form__buttons__sm__f btn btn-outline-dark">
               <div className="form__buttons__sm__f__content">
                 <div className="form__buttons__sm__f__content--icon">
-                  <i className="fa-brands fa-facebook"></i>
+                  <BrandIcon
+                    iconType={faFacebook}
+                    colorIcon="black"
+                    sizeIcon="12px"
+                  />
                 </div>
                 <div className="form__buttons__sm__f__content--text">
-                  Continua con Facebook
+                  <span>Continua con Facebook</span>
                 </div>
               </div>
             </button>
@@ -80,10 +106,14 @@ const LoginModal = () => {
               className="form__buttons__sm__g btn btn-outline-dark">
               <div className="form__buttons__sm__g__content">
                 <div className="form__buttons__sm__g__content--icon">
-                  <i className="fa-brands fa-google"></i>
+                  <BrandIcon
+                    iconType={faGoogle}
+                    colorIcon="black"
+                    sizeIcon="12px"
+                  />
                 </div>
-                <div lass="form__buttons__sm__g__content--text">
-                  Continua con Google
+                <div className="form__buttons__sm__g__content--text">
+                  <span>Continua con Google</span>
                 </div>
               </div>
             </button>
@@ -92,10 +122,14 @@ const LoginModal = () => {
               className="form__buttons__sm__a btn btn-outline-dark">
               <div className="form__buttons__sm__a__content">
                 <div className="form__buttons__sm__a__content--icon">
-                  <i className="fa-brands fa-apple"></i>
+                  <BrandIcon
+                    iconType={faApple}
+                    colorIcon="black"
+                    sizeIcon="12px"
+                  />
                 </div>
-                <div lass="form__buttons__sm__a__content--text">
-                  Continua con Apple
+                <div className="form__buttons__sm__a__content--text">
+                  <span>Continua con Apple</span>
                 </div>
               </div>
             </button>
@@ -104,10 +138,14 @@ const LoginModal = () => {
               className="form__buttons__sm__em btn btn-outline-dark">
               <div className="form__buttons__sm__em__content">
                 <div className="form__buttons__sm__em__content--icon">
-                  <i className="fa-solid fa-envelope"></i>
+                  <BrandIcon
+                    iconType={faEnvelope}
+                    colorIcon="black"
+                    sizeIcon="12px"
+                  />
                 </div>
-                <div lass="form__buttons__sm__em__content--text">
-                  Continua con el correo electronico
+                <div className="form__buttons__sm__em__content--text">
+                  <span>Continua con el correo electronico</span>
                 </div>
               </div>
             </button>
