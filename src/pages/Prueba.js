8c029653e2f103539902d4ce460cc8c486f2ea45
Manuel Bojaca <manuel.bojaca@outlook.com>
2022-05-15 19:51:01 -0500

    Add module5xx

--- src/pages/Prueba.js
+++ src/pages/Prueba.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Photo from '../components/Photo';
 import cardmd1 from '../images/cards/cardmd1.png';
+import Loader from '../components/Loader';
+import Module5xx from '../components/Modelo5xx';
 
 const Prueba = () => {
+  const images = Loader();
+  const img = new Image();
+  img.src = cardmd1;
+  const width = img.width;
+  const height = img.height;
+
+  const images1 = images.slice(0, 5);
+  const images2 = images.slice(5, 10);
+  console.log('images: ', images);
   return (
     <div>
-      <Photo x="188px" y="240px" src={cardmd1} />
+      <p>{`${img} lol ${height}`}</p>
+      <Photo src={img.src} />
+      <Module5xx album={images1} />
+      <Module5xx album={images2} />
     </div>
   );
 };
 export default Prueba;
+
+/*
+const album = Loader('cabañita');
+  album.map((item => {
+    return <Photo x="200px" y="200px" src={item} />
+  }))
+*/
